765d3709b471bfd3efb30aeec04386be85841341
bismo_softwareseni <bismo@softwareseni.com>
2019-07-24 10:19:23 +0700

    first init

---
+++ ss_team_member.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+        Plugin Name: SoftwareSeni Team Member
+        Description: Objectives - understanding custom post type, post meta, and shortcode details.
+        Version: 1.0
+        Author: Bismoko Widyatno
+    */
+
+
+    /**
+     * --------------------------------------------------------------------------
+     * Main class for this plugin. This class will handle most of the team member 
+     * plugin logic
+     * --------------------------------------------------------------------------
+     **/
+    class SS_Team_Member_Main {
+        function __construct() {
+        }
+        
+    }
+?>
+
